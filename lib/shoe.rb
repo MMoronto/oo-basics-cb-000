7b3a474b99d1b32657dae99f4c823c44f9c28f9d
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-02-22 22:30:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand 
+  end
+  
+  def cobble
+    puts 'says that the shoe has been repaired'
+  end 
+  
+end 
 # Make your shoe class here!
